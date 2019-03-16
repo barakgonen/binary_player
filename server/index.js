68b68df0fcd624e7f545468c140e8df31a94aeb9
Barak Gonen <barakgonen9@gmail.com>
2019-03-16 11:14:24 +0200

    Server works, need to add the session

--- server/index.js
+++ server/index.js
@@ -1,14 +1,19 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const net = require("net");
+const fs = require("fs");
+const path = require("path");
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
+function getRecordPath(interfaceName) {
+    // should be complexer, with more than 1 option
+    return pathToRecordFile = path.join(__dirname, '/binary_records/' + interfaceName + '/1.log');
+}
 app.listen(3000, ()=> {
-    console.log("Server is running on port 3000");
-    
+    console.log("Server is running on port 3000");  
 })
 app.get("/", (req, res, next) => {
     res.send("Hello!");
@@ -21,15 +26,22 @@ app.post("/", function(req, res){
 
     console.log("ServerIP = " + serverIP + ", Target port = " + targetPort + ", interfaceName: " + interfaceName);
     /*
-        check 2 things:
+        check things:
         1. Is server reachable, by ping?
     */
+
 
     res.send(req.body);  
     console.log("Opening socket for writing");
 
-    let client = new net.Socket(targetPort, serverIP, function() {
-        console.log('Connected');
-        client.write('Hello, server! Love, Client!');
+    let pathToRecordFile = getRecordPath(interfaceName);
+    console.log("Playing record from the following path: " + pathToRecordFile);
+    let binaryRecordEncoding = 'utf8';
+    fs.readFile(pathToRecordFile, binaryRecordEncoding, function(err, contents) {
+        console.log(contents);
     })
+    // let client = new net.Socket(targetPort, serverIP, function() {
+    //     console.log('Connected');
+    //     client.write('Hello, server! Love, Client!');
+    // })
 })
